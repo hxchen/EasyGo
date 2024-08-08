ac41b9b92f7f1f42269fc388a95c1ff43b5bc6eb
hxchen <helloworld.vip@gmail.com>
2024-08-08 12:04:38 +0800

    添加web路由和中间件

---
+++ examples/HttpServer.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+func echo(wr http.ResponseWriter, r *http.Request) {
+	msg, err := io.ReadAll(r.Body)
+	if err != nil {
+		wr.Write([]byte("read error"))
+		return
+	}
+	writeLen, err := wr.Write(msg)
+	if err != nil || writeLen != len(msg) {
+		log.Println(err, "write len:", writeLen)
+		return
+	}
+}
+
+func welcome(wr http.ResponseWriter, r *http.Request) {
+	wr.Write([]byte("welcome"))
+}
+
+// timeMiddleware 记录请求处理时间的中间件
+func timeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(wr http.ResponseWriter, r *http.Request) {
+		timeStart := time.Now()
+		next.ServeHTTP(wr, r)
+		timeElapsed := time.Since(timeStart)
+		log.Println("time elapsed:", timeElapsed)
+	})
+}
+
+type Router struct {
+	middlewareChain []middleware
+	mux             map[string]http.Handler
+}
+
+type middleware func(http.Handler) http.Handler
+
+func NewRouter() *Router {
+	return &Router{
+		mux: make(map[string]http.Handler),
+	}
+}
+
+func (r *Router) Use(m middleware) {
+	r.middlewareChain = append(r.middlewareChain, m)
+}
+
+func (r *Router) Add(route string, h http.Handler) {
+	var mergedHandler = h
+	for i := len(r.middlewareChain) - 1; i >= 0; i-- {
+		mergedHandler = r.middlewareChain[i](mergedHandler)
+	}
+	r.mux[route] = mergedHandler
+}
+
+// ServeHTTP 方法使 Router 结构体符合 http.Handler 接口，用于处理传入的 HTTP 请求。
+func (r *Router) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
+	if handler, ok := r.mux[req.URL.Path]; ok {
+		handler.ServeHTTP(wr, req)
+		return
+	}
+	http.NotFound(wr, req)
+}
+
+func main() {
+	r := NewRouter()
+	r.Use(timeMiddleware)
+	r.Add("/welcome", http.HandlerFunc(welcome))
+
+	http.Handle("/", r)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
